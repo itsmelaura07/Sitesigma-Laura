9f20587b1dd2ba9c03795ef7f3b269b268db8c6c
Laura nascente <92733398+itsmelaura07@users.noreply.github.com>
2022-07-22 21:37:57 -0300

    Update usuario-lista.php
    Update

--- Sitesigma-Laura/Admin/usuario-lista.php
+++ Sitesigma-Laura/Admin/usuario-lista.php
@@ -1,9 +1,11 @@
 <?php
-include_once './includes/_bancos.php';
+include_once './includes/_banco.php';
 
 include_once './includes/_header.php';
 
 include_once '../includes/_menu.php';
+
+include_once './includes/_dados.php';
 ?>
 
 
